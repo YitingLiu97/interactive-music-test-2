51e3b566dd9b63969ea39a27259be8e8e2cd7a17
YitingInTech <yl3466@nyu.edu>
2025-04-07 15:04:38 +0800

    bounding box changes based on window screen resize

--- my-app/src/Components/BoundingBox.tsx
+++ my-app/src/Components/BoundingBox.tsx
@@ -2,22 +2,32 @@
 import React from "react"
 import DraggableCircle from "./DraggableCircle"
 import { useRef, useEffect, useState } from "react"
+
 export default function BoundingBox() {
     const boxRef = useRef<HTMLDivElement>(null);
     const [size, setSize] = useState({ x: 100, y: 100 });
-    const [mounted, SetMounted] = useState(false);
-
-
-    useEffect(() => SetMounted(true), []);
-
-
+    const [mounted, setMounted] = useState(false);
+    const [resize, setResize] = useState(false);
+    useEffect(() => setMounted(true), []);
     useEffect(() => {
+        function updateSize(){
+        
         if (boxRef.current) {
             const rect = boxRef.current.getBoundingClientRect();
             setSize({ x: rect.width, y: rect.height });
-        }
-    },[mounted])
+            console.log("Size updated:", rect.width, rect.height);
+
+        }}
+        updateSize();
+        window.addEventListener("resize", updateSize);
     
+        return () => {
+            window.removeEventListener("resize", updateSize);
+        };
+
+
+    }, [mounted]);
+
     if (!mounted) return null;
 
     return (
