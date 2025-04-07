c8c7c63e775f920f413a60ee11f5599d3f43070b
YitingInTech <yl3466@nyu.edu>
2025-04-07 17:24:46 +0800

    file reorg circle UI for ui purposes

---
+++ my-app/src/Components/CircleUI.tsx
@@ -0,0 +1,39 @@
+'use client'
+import React from "react"
+type BoundingBox = {
+    x: number,
+    y: number
+}
+type Props = {
+    xPercent: number,
+    yPercent: number;
+    onMouseDown: () => void;
+    isDragging: boolean;
+    boundingBox: BoundingBox;
+    color?: string;
+}
+export default function CircleUI({ xPercent,
+    yPercent,
+    onMouseDown,
+    isDragging,
+    boundingBox,
+    color = "red", 
+}: Props) {
+    const circleSize = 50;
+
+    return (
+        <div
+            onMouseDown={onMouseDown}
+            style={{
+                width: circleSize,
+                height: circleSize,
+                borderRadius: "50%",
+                backgroundColor: `${color}`,
+                position: "absolute",
+                transform: `translate(${(xPercent * boundingBox.x) / 100}px, ${(yPercent * boundingBox.y) / 100}px)`,
+                cursor: isDragging ? "grabbing" : "grab",
+                transition: isDragging ? "none" : "transform 0.1s ease",
+            }}
+        />
+    );
+}
