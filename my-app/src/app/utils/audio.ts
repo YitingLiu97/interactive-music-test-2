6e28704008769a045be0901b9a4ce36a65415469
YitingInTech <yl3466@nyu.edu>
2025-04-07 15:32:28 +0800

    create basic tone functionality

---
+++ my-app/src/app/utils/audio.ts
@@ -0,0 +1,31 @@
+import * as Tone from "tone";
+
+let player: Tone.Player;
+let panner: Tone.Panner;
+let volume: Tone.Volume;
+
+export async function setupPlayer(url:string){
+
+    await Tone.start();
+    player = new Tone.Player(url).toDestination();
+    player.autostart = false;
+}
+
+export function startPlayback(){
+    player.start();
+}
+export function stopPlayback(){
+    player.stop();
+}
+
+export function AdjustVolume(value: number) {
+    volume = new Tone.Volume(value).toDestination();
+    player.connect(volume);
+}
+
+// 0 to 1 
+export function AdjustPanning(value: number) {
+    panner = new Tone.Panner(value).toDestination();
+    player.connect(panner);
+
+}
