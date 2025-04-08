fc5b671985b198f3fc6b94a698b77fe023e0955c
YitingInTech <yl3466@nyu.edu>
2025-04-08 17:24:01 +0800

    adding the audio tracks
    wifi is too slow to test

--- my-app/src/Components/BoundingBox.tsx
+++ my-app/src/Components/BoundingBox.tsx
@@ -2,25 +2,83 @@
 import React from "react"
 import AudioCircle from "./AudioCircle";
 import { useRef, useEffect, useState } from "react"
+import AudioInterface from "./AudioInterface";
+import { useAudioCircle } from "@/app/utils/useAudioCircle";
+interface AudioInfo {
+    audioUrl: string;
+    circleColor?: string;
+}
 
 export default function BoundingBox() {
     const boxRef = useRef<HTMLDivElement>(null);
     const [size, setSize] = useState({ x: 100, y: 100 });
-    const [mounted, setMounted] = useState(false);
-    const [resize, setResize] = useState(false);
-    useEffect(() => setMounted(true), []);
-    useEffect(() => {
-        function updateSize(){
-        
-        if (boxRef.current) {
-            const rect = boxRef.current.getBoundingClientRect();
-            setSize({ x: rect.width, y: rect.height });
-            console.log("Size updated:", rect.width, rect.height);
+    const [mounted, setMounted] = useState<boolean>(false);
+    const [resize, setResize] = useState<number>(50);
+    const [alltracks, setAllTracks] = useState<string[]>();
+    const [singleTrack, setSingleTrack] = useState<string>();
 
-        }}
+     const audioInfos: AudioInfo[] = [
+        {
+            audioUrl: "/resources/piano.mp3",
+            circleColor: "red"
+
+        },
+        {
+            audioUrl: "/resources/bass.mp3",
+            circleColor: "purple"
+
+        },
+        {
+            audioUrl: "/resources/druims.mp3",
+            circleColor: "gray"
+
+        }
+    ];
+
+    useEffect(() => setMounted(true), []);
+
+    // in this case, should i add a ref for the tracks instead 
+    // and how can i push the array when i hav ethe use state? 
+    function initPlayerforAll(){
+        audioInfos.forEach(track=>{
+            useAudioCircle(track.audioUrl).play;
+            alltracks?.push(track.audioUrl);
+        })
+    }
+
+    // audio logic 
+    function playAll() {
+        audioInfos.forEach(track=>{
+            useAudioCircle(track.audioUrl).play;
+        })
+    }
+
+    function pauseAll(){
+        audioInfos.forEach(track=>{
+            useAudioCircle(track.audioUrl).pause;
+        })
+    }
+
+    function toggleAll(){
+        // for looping 
+        audioInfos.forEach(track=>{
+            useAudioCircle(track.audioUrl).toggleLoop;
+        })
+    }
+
+
+    useEffect(() => {
+        function updateSize() {
+            if (boxRef.current) {
+                const rect = boxRef.current.getBoundingClientRect();
+                setSize({ x: rect.width, y: rect.height });
+                console.log("Size updated:", rect.width, rect.height);
+
+            }
+        }
         updateSize();
         window.addEventListener("resize", updateSize);
-    
+
         return () => {
             window.removeEventListener("resize", updateSize);
         };
@@ -40,7 +98,8 @@ export default function BoundingBox() {
                 backgroundColor: "#f0f0f0"
 
             }}>
-            <AudioCircle boundingBox={size} audioUrl="/resources/piano.mp3" color="red"  />
-            </div>
+            <AudioCircle boundingBox={size} audioUrl="/resources/piano.mp3" color="red" />
+            <AudioInterface trackListName="air traffic noise" authorName="alex ruthmann" onPlayAll={playAll} onPauseAll={pauseAll} onToggleAll={toggleAll} />
+        </div>
     )
 }
