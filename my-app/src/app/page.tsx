996a681e693b7e6b3170fabb143e9e54070ce011
YitingInTech <yl3466@nyu.edu>
2025-05-06 11:12:02 +0800

    move test files in tests folder

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,8 @@
 import BoundingBox from "@/Components/BoundingBox";
-import TestTwoHands from "@/Components/TestTwoHands";
 export default function Home() {
   return (
     <div >
-      {/* <HandTracker/> */}
        <BoundingBox/>
-       {/* <TestTwoHands/> */}
-       {/* <HandDetector/> */}
-       {/* <Demo/> */}
     </div>
   );
 }
