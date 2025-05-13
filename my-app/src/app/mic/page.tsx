8ab37b0fb77b918d4458594a8cfb20e3359b1aca
YitingInTech <yl3466@nyu.edu>
2025-05-13 15:17:08 +0800

    remove hello

--- my-app/src/app/mic/page.tsx
+++ my-app/src/app/mic/page.tsx
@@ -2,9 +2,6 @@ import AudioRecorderComponent from "@/Components/AudioRecorderComponent";
 export default function MicInputPage() {
   return (
     <div >
-      Hello
-       {/* <BoundingBox/> */}
-       {/* <MicInputExample/> */}
        <AudioRecorderComponent/>
     </div>
   );
