13cc295289e65b7b24eb5933c39ff4c6ed45503e
YitingInTech <yl3466@nyu.edu>
2025-05-26 20:52:45 +0800

    remove mic test

--- my-app/src/app/mic/page.tsx
+++
@@ -1,8 +0,0 @@
-import AudioRecorderComponent from "@/Components/AudioRecorderComponent";
-export default function MicInputPage() {
-  return (
-    <div >
-       <AudioRecorderComponent width={300} height={500}/>
-    </div>
-  );
-}
